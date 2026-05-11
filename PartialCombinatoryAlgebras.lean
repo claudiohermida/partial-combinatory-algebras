191f5b2fb1b684b023099334a67e83929776a973
Claudio Hermida <claudio.alberto.hermida@protonmail.com>
2026-05-11 17:42:40 -0300

    initial commit

---
+++ PartialCombinatoryAlgebras.lean
@@ -0,0 +1,1 @@
+import PartialCombinatoryAlgebras.Basic
---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+package "partial-combinatory-algebras" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`relaxedAutoImplicit, false⟩,
+    ⟨`maxSynthPendingDepth, .ofNat 3⟩,
+    ⟨`weak.linter.mathlibStandardSet, true⟩,
+  ]
+
+require "leanprover-community" / "mathlib" @ git "v4.29.1"
+
+@[default_target]
+lean_lib «PartialCombinatoryAlgebras» where
+  -- add any library configuration options here
